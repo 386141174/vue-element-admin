c01b1c9102fc2c44fb8f9bb07eb6c1041124217b
liu <386141174@qq.com>
2019-06-19 09:56:52 +0800

    first commit

---
+++ src/views/dashboard/admin/components/TransactionTable.vue
@@ -0,0 +1,55 @@
+<template>
+  <el-table :data="list" style="width: 100%;padding-top: 15px;">
+    <el-table-column label="客户昵称" min-width="200">
+      <template slot-scope="scope">
+        {{ scope.row.realname| orderNoFilter }}
+      </template>
+    </el-table-column>
+    <el-table-column label="时间" width="195" align="center">
+      <template slot-scope="scope">
+        {{ scope.row.add_time | toThousandFilter }}
+      </template>
+    </el-table-column>
+    <el-table-column label="地点" width="100" align="center">
+      <template slot-scope="{row}">
+        <el-tag :type="row.status | statusFilter">
+          {{ row.cityName }}
+        </el-tag>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+import { transactionList } from '@/api/remote-search'
+
+export default {
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        success: 'success',
+        pending: 'danger'
+      }
+      return statusMap[status]
+    },
+    orderNoFilter(str) {
+      return str
+    }
+  },
+  data() {
+    return {
+      list: null
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      transactionList().then(response => {
+        this.list = response.data.slice(0, 8)
+      })
+    }
+  }
+}
+</script>
